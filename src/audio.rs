015416dd3274bba03a62c346ea3ecacc3b2ba094
bogdanov <bbogdan_ov@mail.ru>
2025-07-26 20:24:29 +0300

    reword todo

--- src/audio.rs
+++ src/audio.rs
@@ -1,4 +1,5 @@
-// TODO: audio is currently disabled in the web builds,
+// TODO: make audio work in web builds.
+//       Audio is currently disabled in the web builds,
 //       because i couldn't make `rodio` work on wasm32 builds...
 #[cfg(target_arch = "wasm32")]
 mod wasm;
