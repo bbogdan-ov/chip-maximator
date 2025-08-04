e48ba64e4e3e153633912d7db31f29259d6ceba9
bogdanov <bbogdan_ov@mail.ru>
2025-08-04 21:23:40 +0300

    reword comment

--- src/scene/cartridge_picker/carousel.rs
+++ src/scene/cartridge_picker/carousel.rs
@@ -111,7 +111,7 @@ impl Card {
 			self.lerp_to(target_pos, target_z);
 
 			if self.is_trying_to_drag {
-				// Add small damping
+				// Add subtle damping
 				self.pos.x += ctx.input.mouse_drag_delta().x / 4.0;
 			}
 		}
