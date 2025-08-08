92e134a3c87bb879a88af6abc72ff424b0bfac8c
bogdanov <bbogdan_ov@mail.ru>
2025-08-08 03:10:55 +0300

    fix serif font letter width

--- src/assets.rs
+++ src/assets.rs
@@ -154,10 +154,10 @@ fn serif_font_lookup() -> FontLookup {
 	// /
 	lookup[47] = offset;
 
-	for byte in b" Siljftrcgs-,.!?/".iter() {
+	for byte in b" iljftrcgs-,.!?/".iter() {
 		widths[*byte as usize] = CharWidth::Half;
 	}
-	for byte in b"IJEpeaou0123456789".iter() {
+	for byte in b"IJESpeaou0123456789".iter() {
 		widths[*byte as usize] = CharWidth::ThreeQuarters;
 	}
 
